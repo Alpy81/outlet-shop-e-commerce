8731a65315c99ecd0723ce24257a7f5b51c88c1f
Alpy81 <alperen.ertas@gmx.de>
2025-02-19 22:58:01 +0100

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/outlet-shop-e-commerce/",
-});
+  base: "/github.com/Alpy81/",
+  ...(command !== "serve" && { base: "/outlet-shop-e-commerce/" }),
+}));
